abbe1772804eceb986b0c74a9645056282f0fdf9
Josh <hi@oracularhades.com>
2025-11-20 04:13:00 +1300

    Use NextJS server to serve frontend.

--- server/start-webservers.sh
+++ server/start-webservers.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd /rover-server/guard && /rover-server/guard/guard-server &
 P1=$!
-cd /rover-server && echo "rover!" && ./target/release/lighthouse-server &
+cd /rover-server && echo "lighthouse!" && ./target/release/lighthouse-server &
 P2=$!
-nginx -c /rover-server/nginx/config/split.conf &
+cd /rover-server/frontend && echo "lighthouse frontend!" && npm run start &
 P3=$!
-wait $P1 $P2 $P3
+nginx -c /rover-server/nginx/config/split.conf &
+P4=$!
+wait $P1 $P2 $P3 $P4
