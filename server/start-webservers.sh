f72b6f5b8d04b15ae7f765895cd52bb377722419
Josh <hi@oracularhades.com>
2025-11-20 06:27:57 +1300

    fix pathing

--- server/start-webservers.sh
+++ server/start-webservers.sh
@@ -3,7 +3,7 @@ cd /rover-server/guard && /rover-server/guard/guard-server &
 P1=$!
 cd /rover-server && echo "lighthouse!" && ./target/release/lighthouse-server &
 P2=$!
-cd /rover-server/frontend && echo "lighthouse frontend!" && npm run start &
+cd /rover-server/frontend/lighthouse && echo "lighthouse frontend!" && npm run start &
 P3=$!
 nginx -c /rover-server/nginx/config/split.conf &
 P4=$!
