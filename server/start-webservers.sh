c0a7950e9b0908df851bf8ea4b8fb317d9f32824
Josh <hi@oracularhades.com>
2026-05-29 12:49:28 +1200

    Remove NGINX, update packages, create workflow to update packages every week, remove nginx, fix dockerfile bug

--- server/start-webservers.sh
+++ server/start-webservers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /rover-server/guard && /rover-server/guard/guard-server &
 P1=$!
-cd /rover-server && echo "lighthouse!" && ./target/release/lighthouse-server &
+# The axum server binds the container's exposed port (80) directly, so no
+# reverse proxy is needed in front of it.
+cd /rover-server && echo "lighthouse!" && lighthouse_port=80 ./target/release/lighthouse-server &
 P2=$!
 cd /rover-server/frontend/lighthouse && echo "lighthouse frontend!" && npm run start &
 P3=$!
-nginx -c /rover-server/nginx/config/split.conf &
-P4=$!
-wait $P1 $P2 $P3 $P4
+wait $P1 $P2 $P3
